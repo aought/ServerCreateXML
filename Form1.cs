342e2f93b9c403e1b33a97521ad9b266cd0d8a3e
aought <80733282+aought@users.noreply.github.com>
2021-03-19 11:32:32 +0800

    first commit

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.IO;
@@ -25,76 +26,47 @@ namespace ServerCreateXML
 
         public void CreateXML()
         {
-            XmlDocument xml = new XmlDocument();
+            XmlDocument xmlDoc = new XmlDocument();
 
-            XmlDeclaration xmlDeclaration = xml.CreateXmlDeclaration("1.0", "UTF-8", null);
-            XmlElement root = xml.DocumentElement;
-            xml.InsertBefore(xmlDeclaration, root);
+            XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = xmlDoc.DocumentElement;
+            xmlDoc.InsertBefore(xmlDeclaration, root);
 
-            XmlElement rootElement = xml.CreateElement(String.Empty, "updateFiles", string.Empty);
-            xml.AppendChild(rootElement);
-            XmlElement fileElement = xml.CreateElement(String.Empty, "file", string.Empty);
-            
-            // 配置属性示例
-            fileElement.SetAttribute("name", "text.exe");
-            fileElement.SetAttribute("src", "ftp://192.168.2.113/");
-            fileElement.SetAttribute("version", "999");
-            fileElement.SetAttribute("size", "0");
-            fileElement.SetAttribute("option", "add");
-            rootElement.AppendChild(fileElement);
+            XmlElement rootElement = xmlDoc.CreateElement(String.Empty, "updateFiles", string.Empty);
+            xmlDoc.AppendChild(rootElement);
 
-            xml.Save("E:\\Test\\XML\\test.xml");
+            DirectoryInfo dirInfo = new DirectoryInfo(@ConfigurationManager.AppSettings["updatePath"]);
+            BuildXML(xmlDoc, rootElement, dirInfo);
+            xmlDoc.Save(@ConfigurationManager.AppSettings["config"]);
 
             MessageBox.Show("已生成配置文件");
 
+
         }
 
-        
-
-        public void GetHash()
+        /// <summary>
+        /// 组装XML
+        /// </summary>
+        public void BuildXML(XmlDocument xmlDoc, XmlElement rootElement, DirectoryInfo dirInfo)
         {
-            var dir = new DirectoryInfo(@"E:\Test\EXE");
-            FileInfo[] files = dir.GetFiles();
-            using(SHA256 sha256 = SHA256.Create())
+            foreach (var file in dirInfo.GetFiles())
             {
-                foreach (var fileInfo in files)
-                {
-                    try
-                    {
-                        FileStream fileStream = fileInfo.Open(FileMode.Open);
-                        fileStream.Position = 0;
-                        byte[] hashValue = sha256.ComputeHash(fileStream);
-                        // 目标字段1：文件名
-                        MessageBox.Show($"{fileInfo.FullName}");
-                        // 目标字段2：哈希值
-                        MessageBox.Show(PrintByteArray(hashValue));
-                        fileStream.Close();
+                XmlElement fileElement = xmlDoc.CreateElement(String.Empty, "file", string.Empty);
+                fileElement.SetAttribute("name", file.Name);
+                fileElement.SetAttribute("src", ConfigurationManager.AppSettings["serverURL"]);
+                fileElement.SetAttribute("version", Guid.NewGuid().ToString());
+                fileElement.SetAttribute("size", file.Length.ToString());
+                fileElement.SetAttribute("option", "add");
+                rootElement.AppendChild(fileElement);
 
-                    }
-                    catch (IOException e)
-                    {
-                        MessageBox.Show($"I/O Exception: {e.Message}");
-                    }
-                    catch(UnauthorizedAccessException e)
-                    {
-                        MessageBox.Show($"Access Exception: {e.Message}");
-                    }
-                }
             }
-
+            // 递归子文件夹
+            foreach (var dir in dirInfo.GetDirectories())
+            {
+                BuildXML(xmlDoc, rootElement, dir);
+            }
         }
 
-        public string PrintByteArray(byte[] byteArray)
-        {
-            
-            var sb = new StringBuilder();
-            for (var i = 0; i < byteArray.Length; i++)
-            {
-                var b = byteArray[i];
-                sb.Append(b);
-            }
-            return sb.ToString();
-        }
 
     }
 }
